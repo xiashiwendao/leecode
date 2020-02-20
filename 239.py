fbd1cef7d64ad090fe06147b81ad7129b52784fc
python_gay <xiashiwendao@163.com>
2020-02-21 06:34:48 +0800

    add 239

---
+++ 239.py
@@ -0,0 +1,40 @@
+from collections import deque
+class Solution:
+    def maxSlidingWindow(self, nums: 'List[int]', k: 'int') -> 'List[int]':
+        '''K代表的是窗口大小'''
+        # base cases
+        n = len(nums)
+        if n * k == 0:
+            return []
+        if k == 1:
+            return nums
+        
+        def clean_deque(i):
+            # remove indexes of elements not from sliding window
+            # 这里i-k代表的就是如果队首元素是当前元素的窗口大小前元素，则弹出
+            # 所有的队首元素都是当前窗口的最大元素
+            if deq and deq[0] == i - k:
+                deq.popleft()
+                
+            # remove from deq indexes of all elements 
+            # which are smaller than current element nums[i]
+            while deq and nums[i] > nums[deq[-1]]:
+                deq.pop()
+        
+        # init deque and output
+        deq = deque()
+        max_idx = 0
+        for i in range(k):
+            clean_deque(i)
+            deq.append(i)
+            # compute max in nums[:k]
+            if nums[i] > nums[max_idx]:
+                max_idx = i
+        output = [nums[max_idx]]
+        
+        # build output
+        for i in range(k, n):
+            clean_deque(i)          
+            deq.append(i)
+            output.append(nums[deq[0]])
+        return output
