94300d3ce21346c2a2463f0112e72338f8f28a20
Lorry <xiashiwendao@163.com>
2020-05-13 12:54:31 +0800

    update

--- test.java
+++ test.java
@@ -1,34 +1,46 @@
-class LISRecursion{
-    // 定义一个静态变量 max，用来保存最终的最长的上升子序列的长度
-    static int max;
-    public int f(int[] nums, int n) {
-
-        if (n <= 1) {
-            return n;
-        }
-    
-        int result=0;
-        int maxEndingHere=1; // 以当前数结尾的上升子序列的最长长度
-        // 从头遍历数组，递归求出以每个点为结尾的子数组中最长上升序列的长度
-        for (int i=1; i < n; i++) {
-            result=f(nums, i);
-
-            if (nums[i−1] < nums[n−1] && result + 1 > maxEndingHere) {
-                maxEndingHere=result + 1;
-            }
-        }
-
-        // 判断一下，如果那个数比目前最后一个数要小，那么就能构成一个新的上升子序列 
-        if (max < maxEndingHere) {
-            max=maxEndingHere;
-        }
-
-        // 返回以当前数结尾的上升子序列的最长长度
-        return maxEndingHere;
-    }
-    public int LIS(int[] nums) {
-        max=1;
-        f(nums, nums.length);
-        return max; 
-    }
-}
+double findMedianSortedArrays(int nums1[], int nums2[]) {
+        int m = nums1.length;
+        int n = nums2.length;
+      
+        int k = (m + n) / 2;
+      
+        if ((m + n) % 2 == 1) {
+            return findKth(nums1, 0, m - 1, nums2, 0, n - 1, k + 1);
+          } else {
+            return (
+                findKth(nums1, 0, m - 1, nums2, 0, n - 1, k) + 
+                findKth(nums1, 0, m - 1, nums2, 0, n - 1, k + 1)
+            ) / 2.0;
+        }
+    }
+    
+    double findKth(int[] nums1, int l1, int h1, int[] nums2, int l2, int h2, int k) {
+        int m = h1 - l1 + 1;
+        int n = h2 - l2 + 1;
+      
+        if (m > n) {
+            return findKth(nums2, l2, h2, nums1, l1, h1, k);
+        }
+      
+        if (m == 0) {
+            return nums2[l2 + k - 1];
+        }
+      
+        if (k == 1) {
+            return Math.min(nums1[l1], nums2[l2]);
+        }
+      
+        int na = Math.min(k/2, m);
+        int nb = k - na;
+        int va = nums1[l1 + na - 1];
+        int vb = nums2[l2 + nb - 1];
+      
+        if (va == vb) {
+            return va;
+        } else if (va < vb) {
+            return findKth(nums1, l1 + na, h1, nums2, l2, l2 + nb - 1, k - na);
+        } else {
+           return findKth(nums1, l1, l1 + na - 1, nums2, l2 + nb, h2, k - nb);
+        }
+      
+    }
